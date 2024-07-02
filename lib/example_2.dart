4206401b09576d9b205d86020c9471d8c78db670
Md Faisal Ahamed <faisalah331@gmail.com>
2024-07-02 18:20:36 +0600

    commit

--- lib/example_2.dart
+++ lib/example_2.dart
@@ -9,47 +9,74 @@ class ExampleTwo extends StatefulWidget {
   State<ExampleTwo> createState() => _ExampleTwoState();
 }
 
+extension on VoidCallback{
+  Future<void> delayed(Duration duration) => Future.delayed(duration,this);
+}
+
 class _ExampleTwoState extends State<ExampleTwo>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation _animation;
+    with TickerProviderStateMixin {
+  late AnimationController _counterClockWiseRotationController;
+  late AnimationController _flipController;
+  late Animation _counterClockWiseRotationAnimation;
+  late Animation _flipAnimation;
 
   @override
   void initState() {
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2))
-          ..repeat();
-    _animation = Tween(begin: 0.0, end: 2 * pi).animate(_controller);
+    const Duration(seconds: 1);
+    _counterClockWiseRotationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _counterClockWiseRotationAnimation = Tween(begin: 0.0, end: -(pi / 2))
+        .animate(CurvedAnimation(parent: _counterClockWiseRotationController, curve: Curves.bounceOut));
+
+    _flipController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    _flipAnimation = Tween(begin: 0.0, end: -(pi))
+        .animate(CurvedAnimation(parent: _flipController, curve: Curves.bounceOut));
+
+    _counterClockWiseRotationController.addStatusListener((status){
+      if(status == AnimationStatus.completed){
+        _flipAnimation = Tween(begin: _counterClockWiseRotationAnimation.value, end: _counterClockWiseRotationAnimation.value+pi)
+            .animate(CurvedAnimation(parent: _flipController, curve: Curves.bounceOut));
+
+        _flipController..reset()..forward();
+
+      }
+    });
     super.initState();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _counterClockWiseRotationController.dispose();
+    _flipController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black12,
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            return Transform(
-              transform: Matrix4.identity()..rotateY(_animation.value),
-              alignment: Alignment.center,
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(colors: [Colors.white,Colors.yellow],stops: [.5,.5]),
-                  shape: BoxShape.circle
-                ),
-              ),
-            );
-          },
+    _counterClockWiseRotationController..repeat()..reset()..forward.delayed(const Duration(seconds: 1));
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: AnimatedBuilder(
+              animation: _counterClockWiseRotationAnimation,
+              builder: (context, child) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateZ(_counterClockWiseRotationAnimation.value),
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        // color: Colors.blue
+                        gradient: LinearGradient(
+                            colors: [Colors.blue, Colors.yellow],
+                            stops: [.5, .5])),
+                  ),
+                );
+              }),
         ),
       ),
     );
